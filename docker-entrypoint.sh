4f3f4acf3250d334014ed0d241317e398d3d152a
Mohammad Amin <dadgaramin96@gmail.com>
2025-02-27 10:16:16 +0330

    fix: update coverage command to ignore parsing errors!

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 python3 -m coverage run --omit=tests/* -m pytest . && echo "Tests Passed" || exit 1
-python3 -m coverage lcov -o coverage/lcov.info
+python3 -m coverage lcov -i -o coverage/lcov.info
