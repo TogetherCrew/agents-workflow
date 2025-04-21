46113756c1a2ba966d498dc8592e86b557c97095
Mohammad Amin <dadgaramin96@gmail.com>
2025-04-21 11:39:51 +0330

    feat: clean up code structures!

--- tasks/agent.py
+++ tasks/agent.py
@@ -1,66 +1,11 @@
-import logging
 from datetime import timedelta
 
-from temporalio import activity, workflow
+from temporalio import workflow
 from temporalio.common import RetryPolicy
 
 with workflow.unsafe.imports_passed_through():
-    from crewai.crews.crew_output import CrewOutput
-    from tasks.hivemind.agent import AgenticHivemindFlow
-    from tasks.redis_memory import RedisMemory
     from tc_temporal_backend.schema.hivemind import HivemindQueryPayload
-
-
-@activity.defn
-async def run_hivemind_agent_activity(
-    payload: HivemindQueryPayload,
-) -> str | None:
-    """
-    Activity that instantiates and runs the Crew.ai Flow (AgenticHivemindFlow).
-    It places the resulting answer into payload.content.response.
-    """
-
-    memory: RedisMemory | None
-    chat_history: str | None
-
-    if payload.chat_id:
-        memory = RedisMemory(key=f"conversation:{payload.chat_id}")
-        chat_history = memory.get_text()
-    else:
-        chat_history = None
-        memory = None
-
-    # Instantiate the flow with the user query
-    flow = AgenticHivemindFlow(
-        community_id=payload.community_id,
-        user_query=payload.query,
-        enable_answer_skipping=payload.enable_answer_skipping,
-        chat_history=chat_history,
-    )
-
-    # Run the flow
-    crew_output = await flow.kickoff_async(inputs={"query": payload.query})
-
-    if isinstance(crew_output, CrewOutput):
-        final_answer = crew_output.raw
-    elif not payload.enable_answer_skipping:
-        final_answer = "No answer was generated."
-    else:
-        final_answer = None
-
-    if isinstance(final_answer, str) and "encountered an error" in final_answer.lower():
-        logging.error(f"final_answer: {final_answer}")
-        final_answer = "Looks like things didn't go through. Please give it another go."
-
-    if memory and final_answer != "NONE":
-        chat = f"User: {payload.query}\nAgent: {final_answer}"
-        memory.append_text(chat)
-
-    if final_answer == "NONE":
-        return None
-    else:
-        return final_answer
-
+    from tasks.hivemind.activities import run_hivemind_agent_activity
 
 @workflow.defn
 class AgenticHivemindTemporalWorkflow:
